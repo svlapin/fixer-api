cdbfacd988f856e370169ce5450f3d24d49ee257
Sergey Lapin <sv0lapin@gmail.com>
2017-07-25 08:23:04 +0300

    drop lodash dependency

--- lib/Fixer.ts
+++ lib/Fixer.ts
@@ -1,6 +1,5 @@
 'use strict';
 
-import { isString, isDate, pick } from 'lodash';
 import * as moment from 'moment';
 
 export interface IFixerRates {
@@ -27,19 +26,21 @@ export abstract class Fixer {
     let formattedDate;
 
     const RE_DATE = /^\d{4}-\d{2}-\d{2}$/;
-    if (isString(date) && RE_DATE.test(date)) {
+    if (typeof date === 'string' && RE_DATE.test(date)) {
       formattedDate = date;
-    } else if (isDate(date)) {
+    } else if (date instanceof Date) {
       formattedDate = moment(date).format('YYYY-MM-DD');
     } else {
       throw new Error('Invalid date argument');
     }
 
-    return this.request(`/${formattedDate}`, pick(opts, 'base', 'symbols'));
+    const { base, symbols } = opts;
+    return this.request(`/${formattedDate}`, { base, symbols });
   }
 
   latest(opts: any = {}) {
-    return this.request('/latest', pick(opts, 'base', 'symbols'));
+    const { base, symbols } = opts;
+    return this.request('/latest', { base, symbols });
   }
 
   protected abstract request(url: string, opts: any): Promise<IFixerResponse>;
