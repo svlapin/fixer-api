35265d57e29d18e8b3ee705276d8b74f0060ee5f
Sergey Lapin <sv0lapin@gmail.com>
2017-07-26 00:18:41 +0300

    attempt to fix missing coverage

--- lib/NodeFixer.ts
+++ lib/NodeFixer.ts
@@ -10,7 +10,7 @@ class NodeFixer extends Fixer {
       ('https:' === this.baseUrl.protocol ? https.get : http.get)
         ({ ...this.baseUrl, path }, (res) => {
           const contentType = res.headers['content-type'];
-          let error;
+          let error: Error;
 
           if (res.statusCode !== 200) {
             error = new Error(`Request failed: status code ${res.statusCode}`);
