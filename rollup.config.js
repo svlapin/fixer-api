dd4ee1ac0fd1ac3944f8b4a1cf8844b1eece3236
Sergey Lapin <sv0lapin@gmail.com>
2019-04-20 20:39:59 +0200

    chore: get rid of babel in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 
 const extensions = [
@@ -16,10 +15,7 @@ export default {
 
   plugins: [
     resolve({ extensions }),
-
-    commonjs(),
-
-    babel({ extensions }),
+    commonjs()
   ],
 
   output: [{
