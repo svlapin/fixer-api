231e114609eb2c3d9ede5ad847d384d53ea0dad3
Sergey Lapin <sv0lapin@gmail.com>
2025-01-11 08:22:07 +0100

    chore: update @rollup/plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
 
